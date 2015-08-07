cd32134b2663591685a3f38216038b27f0dae818
Chris McKnight <cmckni3@gmail.com>
2015-08-06 23:38:24 -0500

    Fixes pecl install for travis

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -4,6 +4,6 @@ set -ex
 curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.2.tar.gz | tar zx
 cd libsodium-1.0.2 && ./configure --prefix=$HOME/libsodium && make && make check
 make install
-pecl install -f libsodium-0.1.3
+LIBSODIUM_DIR=$HOME/libsodium pecl install -f libsodium-0.1.3
 composer self-update
 
