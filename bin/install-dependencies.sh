e787701c94465829c849ea3eb904ed95a9124eb1
Chris McKnight <cmckni3@gmail.com>
2015-08-06 23:42:04 -0500

    Adds caching to Travis build

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -ex
 
-curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.2.tar.gz | tar zx
-cd libsodium-1.0.2 && ./configure --prefix=$HOME/libsodium && make && make check
-make install
-LIBSODIUM_DIR=$HOME/libsodium pecl install -f libsodium-0.1.3
+LIBSODIUM_DIR=$HOME/libsodium
+
+# caching
+if [ ! -d "$LIBSODIUM_DIR/lib" ]; then
+  curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.2.tar.gz | tar zx
+  cd libsodium-1.0.2 && ./configure --prefix=$LIBSODIUM_DIR && make && make check
+  make install
+else
+  echo 'Using cached directory.'
+fi
+
+LIBSODIUM_DIR=$LIBSODIUM_DIR pecl install -f libsodium-0.1.3
 composer self-update
 
